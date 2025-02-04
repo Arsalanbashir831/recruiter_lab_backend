5d7d434291c4cba9af670e8e41f22138839b3318
huzaifa003 <buntyhuzaifa3@gmail.com>
2025-02-05 00:19:27 +0500

    Added Refresh and Verify Token

--- users/urls.py
+++ users/urls.py
@@ -5,10 +5,13 @@ from .views import (
     UserUpdateView, UserDeleteView, GoogleAuthView,
 )
 
+from rest_framework_simplejwt.views import TokenVerifyView, TokenRefreshView
+
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+
     path('verify-otp/', VerifyOTPView.as_view(), name='verify-otp'),
     path('resend-otp/', ResendOTPView.as_view(), name='resend-otp'),
     path('request-password-reset/', RequestPasswordResetView.as_view(), name='request-password-reset'),
@@ -20,4 +23,7 @@ urlpatterns = [
     path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
     path('<int:pk>/update/', UserUpdateView.as_view(), name='user-update'),
     path('<int:pk>/delete/', UserDeleteView.as_view(), name='user-delete'),
+
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
